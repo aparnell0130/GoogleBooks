328d124470b6713135a947c42bfa05a2a27148a8
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-12 15:51:32 -0800

    added state to search page and props to search body

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBody from '../components/searchBody/SearchBody'
+import API from '../utils/API'
 
 
 function Search() {
+    const [search, setSearch] = useState('')
+    const [books, setBooks] = useState([])
+    const searchInput = (event) => {
+        setSearch(event.target.value)
+    }
+    const searchTitle = (event) => {
+        event.preventDefault()
+        API.getSearch(search)
+            .then(res => setBooks(res.items))
+    }
     return (
         <div>
-            <SearchBody />
+            <SearchBody books={books} searchInput={searchInput} searchTitle={searchTitle} />
         </div>
     )
 }
