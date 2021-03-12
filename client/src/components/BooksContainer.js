57c8e9dcbe675a011ba63d25800a24d745e9a4b1
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-12 12:08:28 -0800

    added starting components

---
+++ client/src/components/BooksContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from './body/Body'
+import Header from './Header/Header'
+
+function BooksContainer() {
+    return (
+        <div>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+export default BooksContainer
