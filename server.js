47016bea5f19e5796b1e57d5fc390d984c5e4f7e
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-12 11:25:22 -0800

    updated server.js with starting code

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, function () {
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
