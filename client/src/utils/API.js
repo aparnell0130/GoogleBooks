e1a92223595127084422376e7a480e15bc3bca59
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-15 00:23:46 -0700

    added axios post route for save book

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,9 @@ import axios from 'axios'
 const API = {
     getSearch: (search) => {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${search}`)
+    },
+    saveBook: (data) => {
+        return axios.post('/api/books', data)
     }
 }
 
