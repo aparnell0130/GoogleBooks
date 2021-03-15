162071282dd84ef80e45f04714d4c4481e71cc6b
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-15 15:36:01 -0700

    updated search body to show results if results no results if no results

--- client/src/components/searchBody/SearchBody.js
+++ client/src/components/searchBody/SearchBody.js
@@ -20,49 +20,54 @@ function SearchBody({ books, searchInput, searchTitle }) {
                     </Card.Body>
                 </Card>
                 <Card className='mt-4'>
-                    <Card.Header as='h5'>Results</Card.Header>
-                    {books.map(book => (
-                        <Card.Body key={book.id}>
-                            <Card>
-                                <Card.Body>
-                                    <Row >
-                                        <Col md={6}>
-                                            <Card.Title >
-                                                {book.volumeInfo.title}
-                                            </Card.Title>
-                                        </Col>
-                                        <Col md={{ span: 2, offset: 4 }}>
-                                            <Button className='m-1'>View</Button>
-                                            <Button className='m-1'>Save</Button>
-                                        </Col>
+                    {books.length ? (
+                        <div>
+                            <Card.Header as='h5'>Results</Card.Header>
+                            {books.map(book => (
+                                <Card.Body key={book.id}>
+                                    <Card>
+                                        <Card.Body>
+                                            <Row >
+                                                <Col md={6}>
+                                                    <Card.Title >
+                                                        {book.volumeInfo.title}
+                                                    </Card.Title>
+                                                </Col>
+                                                <Col md={{ span: 2, offset: 4 }}>
+                                                    <Button className='m-1'>View</Button>
+                                                    <Button className='m-1'>Save</Button>
+                                                </Col>
+                                            </Row>
+                                            <Card.Subtitle className='mt-2'>{book.volumeInfo.authors}</Card.Subtitle>
+                                            <Row>
+                                                <Col className='my-2'>
+                                                    <Card.Img
+                                                        src={
+                                                            book.volumeInfo.imageLinks === undefined
+                                                                ? 'https://placehold.it/300x300'
+                                                                : `${book.volumeInfo.imageLinks.thumbnail}`
+                                                        }
+                                                        style={{ width: '200px', height: '250px' }}
+                                                    />
+                                                </Col>
+                                                <Col md={9}>
+                                                    <Card.Text>
+                                                        {book.volumeInfo.description}
+                                                    </Card.Text>
+                                                </Col>
 
+                                            </Row>
 
-                                    </Row>
-
-                                    <Card.Subtitle className='mt-2'>{book.volumeInfo.authors}</Card.Subtitle>
-                                    <Row>
-                                        <Col className='my-2'>
-                                            <Card.Img
-                                                src={
-                                                    book.volumeInfo.imageLinks === undefined
-                                                        ? 'https://placehold.it/300x300'
-                                                        : `${book.volumeInfo.imageLinks.thumbnail}`
-                                                }
-                                                style={{ width: '200px', height: '250px' }}
-                                            />
-                                        </Col>
-                                        <Col md={9}>
-                                            <Card.Text>
-                                                {book.volumeInfo.description}
-                                            </Card.Text>
-                                        </Col>
-
-                                    </Row>
-
+                                        </Card.Body>
+                                    </Card>
                                 </Card.Body>
-                            </Card>
-                        </Card.Body>
-                    ))}
+                            ))}
+                        </div>
+
+                    ) : (
+                        <Card.Header as='h5'>No Results</Card.Header>
+                    )}
+
                 </Card>
             </Container>
         </div >
