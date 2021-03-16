c762a15df2bf0427d54cb67c042c85dce818b88d
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-03-15 19:23:44 -0700

    added page not found page

--- client/src/components/navbar/Navigation.js
+++ client/src/components/navbar/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
     return (
         <div>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Google Books</Navbar.Brand>
+                <Navbar.Brand >Google Books</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="/">Search</Nav.Link>
                     <Nav.Link href="/saved">Saved</Nav.Link>
